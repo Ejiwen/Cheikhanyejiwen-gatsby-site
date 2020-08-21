374481101dcf2e3a06171962c4bea8347bbcc35d
Cheikhani <skibnewrim@gmail.com>
2020-08-21 07:06:41 -0700

    set up env variable and create a folder for images

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   plugins: [
     "gatsby-plugin-theme-ui",
@@ -23,7 +27,7 @@ module.exports = {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: `6237eox9dm6z`,
-        accessToken: `NTl2ySG7s74M4rHEUAD5WhI8CDyDUubYp_NgyftBBqw`,
+        accessToken: process.env.ACCESS_TOKEN,
       },
     },
   ],
